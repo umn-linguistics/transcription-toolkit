0c31da91cb0be391b21daba59f86e511def969ec
enfrank <enschfrank@gmail.com>
2025-12-26 08:26:58 -0600

    Include validation html

--- src/gsheets/gsheets-app.ts
+++ src/gsheets/gsheets-app.ts
@@ -3,25 +3,26 @@ import { GASSpreadsheetAdapter } from "./gas-spreadsheet-adapter";
 import { TranscriptSpreadsheet } from "../TranscriptSpreadsheet";
 
 export function validateGraphemes(){
-  // Create adapter
+  // Fetch data
   const spreadsheetAdapter = new GASSpreadsheetAdapter();
   const transcriptSpreadsheet = new TranscriptSpreadsheet(spreadsheetAdapter);
-  
   const transcript = transcriptSpreadsheet.getTranscriptData();
   const orthography = transcriptSpreadsheet.getGraphemeData();
 
+  // Validate graphemes
   const invalidTranscriptions = transcript.validateGraphemes(orthography.profile);
-
+  
   // Update spreadsheet with validation results
   transcriptSpreadsheet.updateTranscriptWithGraphemeValidations(invalidTranscriptions, transcript.headers);
 
-//   // Use custom HTML dialog for better display of multiple IDs
-//   const template = HtmlService.createTemplateFromFile('validation-results-template');
-//   template.count = invalidTranscriptions.length;
-//   template.validationType = "character";
-//   template.ids = invalidTranscriptions.map(r => `ID "${r.id}: ${r.text}`);
-//   const html = template.evaluate()
-//     .setWidth(500)
-//     .setHeight(invalidTranscriptions.length === 0 ? 150 : 400);
-//   SpreadsheetApp.getUi().showModalDialog(html, 'Grapheme Validation Results');
+  // Display results in html
+  const template = HtmlService.createTemplateFromFile('validation-results-template');
+  template.count = invalidTranscriptions.length;
+  template.validationType = "character";
+  template.ids = invalidTranscriptions.map(r => `ID ${r.id}: ${r.text}`);
+  Logger.log(`creating html template for displaying validation results...`);
+  const html = template.evaluate()
+    .setWidth(500)
+    .setHeight(invalidTranscriptions.length === 0 ? 150 : 400);
+  SpreadsheetApp.getUi().showModalDialog(html, 'Grapheme Validation Results');
 }
